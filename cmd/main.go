520f623cbdcc125fe68895d4ca75e4ef6ebc2c45
Yorshik <kirkidzapp@gmail.com>
2024-12-22 20:45:50 +0300

    Soltuion

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Yorshik/final_task_sprint_1/internal/server"
+)
+
+func main() {
+	server.StartServer()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Yorshik/final_task_sprint_1
+
+go 1.23.0
